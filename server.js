e8d79eda4d5410bd89f57e1cafe1b2b6779740fc
chedenis <chesdenison@gmail.com>
2024-11-09 09:00:18 -0700

    allow cross origin requests

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.use(
     }),
     cookie: {
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 1000 * 60 * 60 * 48, // 48 hours
     },
   })
