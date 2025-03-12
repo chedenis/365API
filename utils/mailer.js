11710607ff953ffd13ceb249dad108ebb24b5da4
Prince Bhut <pbhut@sigmasolve.com>
2025-03-12 14:07:29 +0530

    changes for email

--- utils/mailer.js
+++ utils/mailer.js
@@ -102,132 +102,10 @@ const sendEmailOTP = async (to, subject, otp, role) => {
   }
 };
 
-const sendEmailForClubCommentsToAdmin = async (
+const sendEmailForClubComments = async (
   to,
   subject,
-  role = "club",
-  extraData = {}
-) => {
-  try {
-    // Create an SES client
-    const sesClient = new SESClient();
-
-    const htmlContent = `
-      <!DOCTYPE html>
-<html>
-
-<head>
-    <title>Reply from Club Owner</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            padding: 20px;
-            color: #333;
-            background-color: rgba(55, 197, 151, 0.2);
-        }
-
-        .container {
-            max-width: 800px;
-            background-color: #ffffff;
-            margin: 0 auto;
-            padding: 40px;
-            border: 1px solid #ddd;
-            border-radius: 8px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            text-align: center;
-        }
-
-        .title {
-            font-size: 26px;
-            font-weight: bold;
-            color: #37C597;
-        }
-
-        .content {
-            margin-top: 15px;
-            line-height: 1.6;
-            font-size: 20px;
-        }
-
-        .time {
-            font-size: 14px;
-        }
-
-        .button {
-            border: 1px solid #37C597;
-            padding: 10px 30px;
-            font-size: 16px;
-            color: white;
-            background-color: #37C597;
-            border-radius: 5px;
-            cursor: pointer;
-            font-weight: 600;
-        }
-
-        .button:hover {
-            background-color: white;
-            color: #37C597;
-        }
-
-        .footer {
-            margin-top: 20px;
-            font-size: 12px;
-            color: #777;
-            text-align: center;
-        }
-    </style>
-</head>
-
-<body>
-    <div class="container">
-        <div class="title">Club ${extraData?.clubName} has replied to your comment</div>
-        <div class="content">
-            <p>We have updated the club details as requested.</p>
-            <p class="time"><b></b>Timestaml</b> ${extraData?.sendingTime}</p>
-            <button class="button">Go to Dashboard</button>
-
-        </div>
-        <div class="footer">
-            Thanks,<br>
-            The 365Dink Team
-        </div>
-    </div>
-</body>
-
-</html>
-    `;
-
-    const emailParams = {
-      Destination: {
-        ToAddresses: [to],
-      },
-      Message: {
-        Body: {
-          Html: {
-            Data: htmlContent,
-          },
-        },
-        Subject: {
-          Data: subject,
-        },
-      },
-      Source: '"365Dink Support" <noreply@365Dink.com>', // Replace with your SES-verified email/domain
-    };
-
-    // Send the email
-    const command = new SendEmailCommand(emailParams);
-    await sesClient.send(command);
-    console.log("Email sent successfully via SES.");
-  } catch (error) {
-    console.error("Error sending email via SES:", error);
-    throw new Error("Unable to send email.");
-  }
-};
-
-const sendEmailForClubCommentsToClubOwner = async (
-  to,
-  subject,
-  role = "club",
+  senderRole = "club", // admin/club
   extraData = {}
 ) => {
   try {
@@ -300,18 +178,33 @@ const sendEmailForClubCommentsToClubOwner = async (
     </style>
 </head>
 
-<body>
+<body style="font-family: Arial, sans-serif;
+            padding: 20px;
+            color: #333;
+            background-color: rgba(55, 197, 151, 0.2);" >
     <div class="container">
-        <div class="title">Your club details have been rejected by Admin</div>
+      ${
+        senderRole == "club"
+          ? `<div class="title">Your club details have been rejected by Admin</div>
         <div class="content">
             <p>Please update the address and contact information.</p>
-            <p class="time"><b></b>Timestaml</b> ${extraData?.sendingTime}</p>
-            <button class="button">Go to Dashboard</button>
+            <a href="${extraData?.redirectUrl}" class="button" style="cursor:pointer;text-decoration:none;">Go to ${extraData?.clubName}</a>
         </div>
         <div class="footer">
             Thanks,<br>
             The 365Dink Team
+        </div>`
+          : `<div class="title">Club ${extraData?.clubName} has replied to your comment</div>
+        <div class="content">
+            <p>We have updated the club details as requested.</p>
+            <a href="${extraData?.redirectUrl}" class="button" style="cursor:pointer;text-decoration:none;">Go to ${extraData?.clubName}</a>
+
         </div>
+        <div class="footer">
+            Thanks,<br>
+            The 365Dink Team
+        </div>`
+      }
     </div>
 </body>
 
@@ -348,6 +241,5 @@ const sendEmailForClubCommentsToClubOwner = async (
 module.exports = {
   sendEmail,
   sendEmailOTP,
-  sendEmailForClubCommentsToAdmin,
-  sendEmailForClubCommentsToClubOwner,
+  sendEmailForClubComments,
 };
