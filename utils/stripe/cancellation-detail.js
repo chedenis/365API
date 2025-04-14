74a70312189cb533b0228d66f82403ac06af62bf
Bhavya Bhojani <bbhojani@sigmasolve.net>
2025-04-14 15:40:17 +0530

    stripe payemnt cancel membership check

--- utils/stripe/cancellation-detail.js
+++ utils/stripe/cancellation-detail.js
@@ -3,11 +3,11 @@ const { stripe } = require("../../config/stripe");
 const { User } = require("../../models");
 
 exports.calculateCancellationDetails = (startDate, testMode = false) => {
-  // const now = dayjs("2026-05-05"); //30-day grace_period
+  const now = dayjs("2026-05-05"); //30-day grace_period
   // const now = dayjs("2026-08-05"); //before_6_months
   // const now = dayjs("2026-12-05"); //after_6_months
 
-  const now = dayjs(); //current time period
+  // const now = dayjs(); //current time period
   const renewalDate = startDate.add(1, "year");
   const gracePeriodEnd = renewalDate.add(30, "day");
   const sixMonthMark = renewalDate.add(6, "month");
